fec53dd32d7ef2515ed578c04fbfc151ef70852e
rishikaushal2009 <rishikaushal2009@gmail.com>
2020-11-08 20:57:39 +0530

    v1 api

--- main.go
+++ main.go
@@ -11,9 +11,8 @@ import (
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/fields"
 	"k8s.io/client-go/kubernetes"
-	api "k8s.io/client-go/pkg/api"
 
-	//v1 "k8s.io/api/batch/v1"
+	v1 "k8s.io/api/batch/v1"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/tools/clientcmd"
